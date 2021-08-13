3584fa213e02e6c702f69ebda0b40cdffe288631
stef5924 <stef5924@ZBC-S-STEF5924>
2021-08-13 09:52:03 +0200

    Add project files.

---
+++ IGANG MED LØKKER/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace IGANG_MED_LØKKER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // i used a "for" loop, i put 0-99 by useing 100 since it stops just before 100, so it becomes 99
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
